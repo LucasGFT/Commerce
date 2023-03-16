388f5d3be2abe90b74152e76163e95516d1ce98c
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2023-03-15 21:06:14 -0300

    conclusao ate o desafio 19

---
+++ challenges/desafio17.js
@@ -0,0 +1,8 @@
+db.createCollection("resumoProdutos");
+
+db.resumoProdutos.insertOne({
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.find().count(),
+});
+
+db.resumoProdutos.find({}, { _id: 0, franquia: 1, totalProdutos: 1 });
